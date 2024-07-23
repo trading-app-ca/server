26a110fb1541d5c950ca83d6d48a6aa6f514cd3b
StructuredTrading <samowen1992@hotmail.com>
2024-07-23 22:44:22 +1000

    fix: Fixed error when trying to create / connect to db

--- src/server.js
+++ src/server.js
@@ -7,4 +7,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
-})
+});
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const DB_URL = process.env.DATABASE_URL;
-
 dotenv.config();
+
+const DB_URL = process.env.DATABASE_URL;
 
 const connectDB = async () => {
     try {
         console.log('DATABASE URL: ', DB_URL);
-        await mongoose.connect(DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(DB_URL || process.env.DATABASE_URL);
         console.log('Database Connected');
     } catch (err) {
         console.error('Error connecting to Database URL: ', err.message);
