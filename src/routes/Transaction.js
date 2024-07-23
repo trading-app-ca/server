85a72f89ac8ea8d1a27430dd322ff1337aad9bae
StructuredTrading <samowen1992@hotmail.com>
2024-07-23 21:31:35 +1000

    feat: Created Transaction routes

---
+++ src/routes/Transaction.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// @route GET /api/transactions
+// @desc Retrieve all transactions for a user
+router.get('/');
+
+module.exports = router;
