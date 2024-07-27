f4dc4105a38066eee8c8f66f298a1e84b31a9805
StructuredTrading <samowen1992@hotmail.com>
2024-07-27 12:25:12 +1000

    feat: Added auth middleware check to Transactions GET route

--- src/routes/Transaction.js
+++ src/routes/Transaction.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 // @route GET /api/transactions
 // @desc Retrieve all transactions for a user
-router.get('/');
+router.get('/', auth);
 
 module.exports = router;
