3fbedb0ae76f7ee31b9d9d90a10f60a814086c17
StructuredTrading <samowen1992@hotmail.com>
2024-07-27 11:54:04 +1000

    feat: Created base API routes in serverConfig, connecting to individual routing files

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,6 +1,8 @@
-const express = require('express');
 const dotenv = require('dotenv');
-const CORS = require('cors');
+const cors = require('cors');
+dotenv.config();
+
+const express = require('express');
 const connectDB = require('./utils/db');
 
 const app = express();
@@ -8,7 +10,20 @@ const app = express();
 // Connect to the Database
 connectDB();
 
-dotenv.config();
-app.use(CORS());    // Enable CORS for all routes
+// Init Middleware
+app.use(express.json());
+app.use(cors());    // Enable CORS for all routes
+
+// Define Routes
+app.use('/api/auth', require('./routes/Auth'));
+app.use('/api/user', require('./routes/User'));
+app.use('/api/portfolio', require('./routes/Portfolio'));
+app.use('/api/transactions', require('./routes/Transaction'));
+app.use('/api/trades', require('./routes/Trade'));
+
+// Server Health check route
+app.get('/health', (request, response) => {
+    response.status(200).send('OK');
+});
 
 module.exports = app;
