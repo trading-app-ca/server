299e6bb07d5bf9bc5583f359c34376d5192f3899
StructuredTrading <samowen1992@hotmail.com>
2024-07-28 22:38:22 +1000

    fix: Added appropraite comments to validateLogin middleware

--- src/middleware/validateLogin.js
+++ src/middleware/validateLogin.js
@@ -6,9 +6,16 @@ const allowedFields = ['email', 'password'];
 module.exports = validateLogin = [
     check('email', 'Please include a valid email').isEmail(),
     check('password')
+        // Check password field exists
         .exists().withMessage('Password is required')
-        .bail()  // Stop running validations if the password does not exist
+        
+        // Stop running validations if the password does not exist
+        .bail()
+
+        // Check length of password
         .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+        
+        // Check password is not blank spaces
         .custom(value => {
             // Check that the password is not just blank spaces
             if (value?.trim().length === 0) {
@@ -16,9 +23,12 @@ module.exports = validateLogin = [
             }
             return true;
         }),
+
+    // Check Body does not contain extra fields
     body().custom(body => {
         const extraFields = Object.keys(body).filter(field => !allowedFields.includes(field));
         if (extraFields.length > 0) {
+            // Log extra fields to server console and return to client
             throw new Error(`Extra fields are not allowed: ${extraFields.join(', ')}`);
         }
         return true;
@@ -26,6 +36,7 @@ module.exports = validateLogin = [
     (request, response, next) => {
         const errors = validationResult(request);
         if(!errors.isEmpty()) {
+            // Log validation errors in server and return to client
             const errorMessages = errors.array().map(error => error.msg);
             console.log(`Invalid login request: \n\t\t\t${errorMessages.join(',\n\t\t\t')}`);
             return response.status(400).json({ msg:  errorMessages });
