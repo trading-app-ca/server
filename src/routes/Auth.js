70fc0c013b02d50bf52fc80b4cc3559cd00adc6e
StructuredTrading <samowen1992@hotmail.com>
2024-07-31 16:20:07 +1000

    feat: Added verify-password route to auth routes and verifyPassword function to auth controller

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const validateLogin = require('../middleware/validateLogin');
 const validateRegister = require('../middleware/validateRegister');
-const { registerUser, loginUser, logoutUser } = require('../controllers/authController');
+const { registerUser, loginUser, logoutUser, verifyPassword } = require('../controllers/authController');
 
 //  @route POST /api/auth/register
 //  @desc Register a new user
@@ -16,5 +16,9 @@ router.post('/login', validateLogin , loginUser);
 // @route POST /api/auth/logout
 // @desc Logout a user
 router.post('/logout', auth, logoutUser);
+
+// @route   POST /api/auth/verify-password
+// @desc    Verify current password
+router.post('/verify-password', auth, verifyPassword);
 
 module.exports = router;
