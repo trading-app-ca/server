22ade0e5154f5662924cbacef59233dc1dc2062a
StructuredTrading <samowen1992@hotmail.com>
2024-08-11 19:34:30 +1000

    fix: removed unnessary information being sent in portfolio schema

--- src/models/Portfolio.js
+++ src/models/Portfolio.js
@@ -9,6 +9,14 @@ const AssetSchema = new mongoose.Schema({
 const PortfolioSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     assets: [AssetSchema]
+},{
+    toJSON: {
+        transform: function (doc, portfolioObject) {
+            delete portfolioObject._id;
+            
+            return portfolioObject
+        }
+    }
 });
 
 module.exports = mongoose.model('Portfolio', PortfolioSchema);
